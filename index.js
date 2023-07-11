93809d6c4c02e08226abdbb1ab3d680cccbc6585
artem_bakhmatov <ip.bakhamtov@mail.ru>
2023-07-11 21:48:09 +0300

    Работа с текстом

--- index.js
+++ index.js
@@ -4,6 +4,38 @@ const token = '6376731335:AAET5cJDyNJl5og5-FXbdgDXzIkLkJEhlfc'; // токен и
 
 const bot = new telegramApi(token, { polling: true });
 
+const chats = {};
+
+const start = () => {
+    bot.setMyCommands([
+        {command: '/start', description: 'Начальное приветствие'},
+        {command: '/info', description: 'Получить информацию о пользователе'},
+        {command: '/game', description: 'Игра угадай цифру'}
+    ]);
+    
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+    
+        if (text === '/start') {
+            await bot.sendSticker(chatId, 'https://telegram.org.ru/uploads/posts/2017-03/1490220353_25.png');
+            return bot.sendMessage(chatId, `Добро пожаловать в телеграм бот Артема Бахматова`);
+        }
+        if (text === '/info') {
+            return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`);
+        }
+        if (text === '/game') {
+            await bot.sendMessage(chatId, 'Сейчас я загадаю цифру от 0 до 9, а ты должен её угадать!');
+            const randomNumber = Math.floor(Math.random() * 10);
+            chats[chatId] = randomNumber;
+            return bot.sendMessage(chatId, 'Отгадывай');
+        }
+
+        return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй ещё раз!'); 
+    });
+}
+
+start();
 
 
 
